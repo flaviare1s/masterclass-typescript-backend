2047bfc2b8bc9193fb33ff4f9355f08fd7861555
flaviare1s <flaviarster@gmail.com>
2025-01-20 15:50:19 -0300

    Cria EmailService

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import EmailService from '../services/EmailService';
 
 const users = [
   { name: 'Diego', email: 'diego@gmail.com' },
@@ -7,5 +8,16 @@ const users = [
 export default {
   async index(req: Request, res: Response) {
     return res.json(users);
+  },
+
+  async create(req: Request, res: Response) {
+    const emailservice = new EmailService();
+
+    emailservice.sendMail({
+      to: { name: 'Diego', email: 'diego@gmail.com' },
+      message: { subject: 'Bem vindo ao sistema', body: 'Seja bem vindo' }
+  });
+
+    return res.send();
   }
 };
